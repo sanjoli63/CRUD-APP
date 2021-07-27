7dd075883dc2767e14a256e8da4a09d40a4317e1
sanjoli63 <sanjoligo6mar@gmail.com>
2021-07-27 12:57:36 +0530

    dailog box

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
+import 'dart:io';
+import 'package:hive/hive.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Directory document = await getApplicationDocumentsDirectory();
+  Hive.init(document.path);
+  await Hive.openBox<String>("Friends");
   runApp(MyApp());
 }
 
@@ -58,11 +67,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Box<String>? friendsBox;
+
   final TextEditingController _idController = TextEditingController();
   final TextEditingController _nameController = TextEditingController();
   @override
   void initState() {
     super.initState();
+    friendsBox = Hive.box<String>("Friends");
   }
 
   Widget build(BuildContext context) {
@@ -76,7 +88,9 @@ class _MyHomePageState extends State<MyHomePage> {
             child: Center(
               child: OutlinedButton(
                 child: Text("Show"),
-                onPressed: () {},
+                onPressed: () {
+                  print(friendsBox!.getAt(0));
+                },
               ),
             ),
           ),
@@ -89,7 +103,39 @@ class _MyHomePageState extends State<MyHomePage> {
                     showDialog(
                         context: context,
                         builder: (_) {
-                          return Dialog();
+                          return Dialog(
+                              child: Container(
+                            padding: EdgeInsets.all(32),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                TextField(
+                                  controller: _idController,
+                                ),
+                                SizedBox(
+                                  height: 16,
+                                ),
+                                TextField(
+                                  controller: _nameController,
+                                ),
+                                SizedBox(
+                                  height: 16,
+                                ),
+                                ElevatedButton(
+                                  onPressed: () {
+                                    final key = _idController.text;
+                                    final value = _nameController.text;
+                                    friendsBox!.put(key, value);
+                                    Navigator.pop(context);
+                                  },
+                                  child: Text(
+                                    "Submit",
+                                    style: TextStyle(color: Colors.teal),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ));
                         });
                   },
                   child: Text("Add New"),
